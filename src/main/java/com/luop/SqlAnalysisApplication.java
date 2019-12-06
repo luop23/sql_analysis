737fbf672f4c1a2fa0a3a983970d4fefb44ed4b5
luoping <892420824@qq.com>
2019-12-06 16:31:06 +0800

    Initial commit

---
+++ src/main/java/com/luop/SqlAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.luop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlAnalysisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luop/SqlAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
