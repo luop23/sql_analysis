49f6db2b0ef7e33933afde57602a705b7ff6b68c
luoping <892420824@qq.com>
2019-12-06 17:20:14 +0800

    修改代码

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 基于jsqlparser 进行sql语句解析，得到字段间的血缘关系
+==
+
+工具说明：
+-
+    1、本工具只解析select语句
+    2、支持子查询
+    3、支持常用数据库的常用函数，如concat、case when、nvl、isnull、cast、ifnull等等
+    4、工具可能对某些sql无法解析，有待完善
+    
+    
+注意事项
+-   
+    1、在调用方法前请确保sql的准确性，本工具只做了简单的判断
+    2、当多表查询时，请给表和字段附上相应别名，如若没有，可能会对结果产生影响
+    
+事例
+-
+    参考测试类 test()
+
+参考网址
+-
+    https://github.com/JSQLParser/JSqlParser
+    https://gitee.com/zzyijia/jsqlparser
+
+
+    
+    
--- src/test/java/com/luop/SqlAnalysisApplicationTests.java
+++ src/test/java/com/luop/SqlAnalysisApplicationTests.java
@@ -3,11 +3,26 @@ package com.luop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Map;
+import java.util.Set;
+
 @SpringBootTest
 class SqlAnalysisApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void test(){
+        String sql = "select u.name userName,u.id,d.name depName,d.id depId " +
+                "from user u left join department d on u.depId = d.id where u.id = 1";
+        try {
+            Map<String, Set<String>> map = SelectParseHelper.getBloodRelationResult(sql);
+            System.out.println(map);   //{depId=[department.id], userName=[user.name], userId=[user.id], depName=[department.name]}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
